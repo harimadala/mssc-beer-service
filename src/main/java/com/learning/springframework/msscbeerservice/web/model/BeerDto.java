8bcc623f0431f4ebb7a0cadf89928ce83facd45a
harimadala <hari.k.madala@gmail.com>
2020-07-16 16:18:16 -0400

    Added ErrorHandler and BeanValidation

--- src/main/java/com/learning/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/learning/springframework/msscbeerservice/web/model/BeerDto.java
@@ -8,25 +8,41 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @NotNull
+    @Positive
     private Long upc;
 
+    @Positive
+    @NotNull
     private BigDecimal price;
 
     private Integer quantityOnHand;
-
 }
